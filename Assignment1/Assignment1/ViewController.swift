a49eab3bfb70509dcc13b9280acd41e359ba4193
Rob McMahon <robert@robs-mbp.attlocal.net>
2019-01-09 11:10:15 -0500

    commiting the xcode project and .gitignore file

---
+++ Assignment1/Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Rob McMahon on 1/9/19.
+//  Copyright © 2019 Otterbein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
